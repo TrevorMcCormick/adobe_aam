4ea2dfd630ff3a5177cdba21da08fb98472f7e3f
Trevor McCormick <trevor.1.mccormick@gm.com>
2021-02-15 04:59:00 +0000

    change data to params

---
+++ aam_api_python/traits/traits.py
@@ -0,0 +1,121 @@
+# Import packages
+import os
+import json
+from datetime import datetime, timedelta
+import requests
+import jwt
+import pandas as pd
+
+class Traits:
+## https://experienceleague.adobe.com/docs/audience-manager/user-guide/api-and-sdk-code/rest-apis/aam-api-getting-started.html?lang=en#optional-api-query-parameters
+
+    @classmethod
+    def get_many(cls,
+                 ## These are all of the Adobe arguments
+                 page=None,
+                 pageSize=None,
+                 sortBy=None,
+                 descending=None,
+                 search=None,
+                 folderId=None,
+                 permissions=None,
+                 includePermissions=None,
+                 ## These are all of the custom arguments
+                 simplify=None
+                 ):
+        ## Traits endpoint
+        request_url = "https://aam.adobe.io/v1/traits/"
+        ## Required headers
+        credentials = eval((os.environ.get('aam_api_credentials')))
+        request_headers = {"Authorization": "Bearer {0}".format(os.environ.get('aam_api_token')),
+                          "x-api-key": credentials['client_id'],
+                          "x-gw-ims-org-id": credentials['org_id']}
+        ## Data includes any arguments if they are declared in the function
+        request_data = {"page":page,
+                        "pageSize":pageSize,
+                        "sortBy":sortBy,
+                        "descending":descending,
+                        "search":search,
+                        "folderId":folderId,
+                        "permissions":permissions,
+                        "includePermissions":includePermissions}
+        ## Make request 
+        response = requests.get(url = request_url,
+                                headers = request_headers,
+                                params = request_data) 
+        ## Print error code if get request is unsuccessful
+        if response.status_code != 200:
+            print(response.content)
+        else:
+            ## Make a dataframe out of the response.json object
+            df = pd.DataFrame(response.json())
+            ## Change time columns from unix time to datetime
+            df['createTime'] = pd.to_datetime(df['createTime'], unit='ms')
+            df['updateTime'] = pd.to_datetime(df['updateTime'], unit='ms')
+            ## This begins the PDM section for additional functionality
+            ## Simplify: limits columns
+            if simplify:
+                df = df[['sid', 'name', 'description', 'dataSourceId', 'folderId']]
+            return df
+
+#     @classmethod
+#     def get_one(cls,
+#           ## These are all of the Adobe arguments
+#           sid,
+#           ## These are all of PDM's custom arguments
+#           simplify=None
+#           ):
+#     ## Traits endpoint for specific trait ID
+#     request_url = "https://aam.adobe.io/v1/traits/{0}".format(str(sid))
+#     ## Required headers
+#     request_headers = {"Authorization": "Bearer {0}".format(token),
+#                       "x-api-key": credentials['clientId'],
+#                       "x-gw-ims-org-id": credentials['orgId']}
+#     ## Make request 
+#     response = requests.get(url = request_url,
+#                             headers = request_headers) 
+#     ## Print error code if get request is unsuccessful
+#     if response.status_code != 200:
+#       print(response.content)
+#     else:
+#       ## Make a dataframe out of the response.json object
+#       df = pd.DataFrame.from_dict(response.json(), orient='index')
+#       df = df.transpose()
+#       ## Change time columns from unix time to datetime
+#       df['createTime'] = pd.to_datetime(df['createTime'], unit='ms')
+#       df['updateTime'] = pd.to_datetime(df['updateTime'], unit='ms')
+#     ## This begins the PDM section for additional functionality
+#     ## Simplify: limits columns
+#     if simplify:
+#       df = df[['sid', 'name', 'description', 'dataSourceId', 'folderId', 'traitRule']]
+#     return df
+
+#   @classmethod
+#   def get_limits(cls):
+#     ## Traits endpoint for limits
+#     request_url = "https://aam.adobe.io/v1/traits/limits"
+#     ## Required headers
+#     request_headers = {"Authorization": "Bearer {0}".format(token),
+#                       "x-api-key": credentials['clientId'],
+#                       "x-gw-ims-org-id": credentials['orgId']}
+#     ## Make request 
+#     response = requests.get(url = request_url,
+#                             headers = request_headers) 
+#     ## Print error code if get request is unsuccessful
+#     if response.status_code != 200:
+#       print(response.content)
+#     else:
+#       ## Uses json_normalize function to make data prettier
+#       json_response = json.loads(response.content.decode('utf-8'))
+#       df = pd.json_normalize(json_response)
+#       df = df.transpose()
+#       return df
+
+#   @classmethod
+#   def create(cls, traits):
+#     ## Required columns
+#     required_columns = pd.DataFrame(columns=['traitType', 'name', 'dataSourceId', 'folderId', 'traitRule', 'ttl', 'description', 'comments'])
+#     df = pd.read_csv(traits, engine='python')
+#     if list(df.columns) != list(required_columns.columns):
+#       required_columns.to_csv('aam_trait_create_template.csv')
+#       raise Exception('Column names are incorrect. Please re-upload file with template.')
