5bad374db376a746a78211face1fb049d72ebb94
Trevor McCormick <trevor.1.mccormick@gm.com>
2021-02-16 03:11:28 +0000

    change traitcreate headers

--- aam_api_python/helpers/headers.py
+++ aam_api_python/helpers/headers.py
@@ -4,9 +4,14 @@ import os
 class Headers:
 
     @classmethod
-    def createHeaders(cls):
+    def createHeaders(cls, traitCreate=False):
         credentials = eval((os.environ.get('aam_api_credentials')))
         request_headers = {"Authorization": "Bearer {0}".format(os.environ.get('aam_api_token')),
                           "x-api-key": credentials['client_id'],
                           "x-gw-ims-org-id": credentials['org_id']}
+        if traitCreate:
+            request_headers = {"Authorization": "Bearer {0}".format(os.environ.get('aam_api_token')),
+                               "x-api-key": credentials['client_id'],
+                               "x-gw-ims-org-id": credentials['org_id'],
+                               "content-type": "application/json"}
         return request_headers
