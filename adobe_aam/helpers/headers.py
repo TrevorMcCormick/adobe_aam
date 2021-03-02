095e0d94ef6129f1f559e4d44c3e3b2e2131bd73
Trevor McCormick <trevor.1.mccormick@gm.com>
2021-03-02 15:05:47 +0000

    change argument traitCreate to json

--- adobe_aam/helpers/headers.py
+++ adobe_aam/helpers/headers.py
@@ -4,15 +4,16 @@ import os
 class Headers:
 
     @classmethod
-    def createHeaders(cls, traitCreate=False):
+    def createHeaders(cls, json=False):
         credentials = eval((os.environ.get('aam_api_credentials')))
         request_headers = {"Authorization": "Bearer {0}".format(os.environ.get('aam_api_token')),
                           "x-api-key": credentials['client_id'],
                           "x-gw-ims-org-id": credentials['org_id']}
-        if traitCreate:
+        if json:
             request_headers = {"accept": "application/json",
+                               "content-type": "application/json",
                                "Authorization": "Bearer {0}".format(os.environ.get('aam_api_token')),
                                "x-api-key": credentials['client_id'],
-                               "x-gw-ims-org-id": credentials['org_id'],
-                               "content-type": "application/json"}
+                               "x-gw-ims-org-id": credentials['org_id']}
+        
         return request_headers
