1b82fa9687d4137204132e1f4079daf30bea9d80
Trevor McCormick <trevor.1.mccormick@gm.com>
2021-03-23 18:11:46 +0000

    merge device and cross-device reporting

--- adobe_aam/reports/reports.py
+++ adobe_aam/reports/reports.py
@@ -62,23 +62,28 @@ class Reports:
                 ## Make a dataframe out of the response.json object
                 raw_data = response.content
                 columns = raw_data.decode('utf-8').replace('"','').split("\n")[0].split(",")
+                for col in range(4, len(columns)):
+                    columns[col] = '{0}_'.format(identity) + columns[col]
                 traits_trend_identity = pd.DataFrame(columns=columns)
                 for i in range(1,len(sid)+1):
                     # The code below handles for trait names with commas in them.
                     try:
                         traits_trend_row = raw_data.decode('utf-8').replace('"','').split("\n")[1].split(",")
                         trait_name = []
-                        for i in traits_trend_row:
-                            if ' ' in i:
-                                trait_name.append(traits_trend_row.index(i))
-                        traits_trend_row_left = traits_trend_row[0:3]
-                        traits_trend_row_mid = traits_trend_row[trait_name[0]:trait_name[-1]+1]
-                        traits_trend_row_mid = ','.join(traits_trend_row_mid)
-                        traits_trend_row_right = traits_trend_row[trait_name[-1]+1:]
-                        traits_trend_name = []
-                        traits_trend_name.append(traits_trend_row_mid)
-                        traits_trend_new = traits_trend_row_left + traits_trend_name + traits_trend_row_right
-                        series = pd.Series(traits_trend_new, index = traits_trend_identity.columns)
+                        if len(traits_trend_row) > 13:
+                            for i in traits_trend_row:
+                                if ' ' in i:
+                                    trait_name.append(traits_trend_row.index(i))
+                            traits_trend_row_left = traits_trend_row[0:3]
+                            traits_trend_row_mid = traits_trend_row[trait_name[0]:trait_name[-1]+1]
+                            traits_trend_row_mid = ','.join(traits_trend_row_mid)
+                            traits_trend_row_right = traits_trend_row[trait_name[-1]+1:]
+                            traits_trend_name = []
+                            traits_trend_name.append(traits_trend_row_mid)
+                            traits_trend_new = traits_trend_row_left + traits_trend_name + traits_trend_row_right
+                            series = pd.Series(traits_trend_new, index = traits_trend_identity.columns)
+                        else:
+                            series = pd.Series(traits_trend_row, index = traits_trend_identity.columns)
                         traits_trend_identity = traits_trend_identity.append(series, ignore_index=True)
                     except:
                         traits_trend_row = raw_data.decode('utf-8').replace('"','').split("\n")[i].split(",")
@@ -86,5 +91,7 @@ class Reports:
             return traits_trend_identity
         
         traits_trend_device = traits_trend_identity('DEVICE')
-        #traits_trend_xdevice = traits_trend_identity('CROSS_DEVICE')
-        return traits_trend_device
+        traits_trend_xdevice = traits_trend_identity('CROSS_DEVICE')
+        traits_trend_xdevice_metrics = traits_trend_xdevice.iloc[:,4:]
+        traits_trend_merged = pd.merge(traits_trend_device, traits_trend_xdevice_metrics, left_index=True, right_index=True)
+        return traits_trend_merged
